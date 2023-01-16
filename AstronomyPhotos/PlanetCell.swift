a63247048e81d9a62e8e098f371dacdcb2a27a2a
Brendon Crowe <brendon.m.crowe@gmail.com>
2023-01-16 17:18:41 -0500

    Updated code

--- AstronomyPhotos/PlanetCell.swift
+++ AstronomyPhotos/PlanetCell.swift
@@ -11,7 +11,16 @@ class PlanetCell: UITableViewCell {
     
     @IBOutlet weak var planetImageView: UIImageView!
     
+    
+    // objective: stop flickering while dequeuing reusable cells
+    // solution: keep track of the image url string by using a string variable for the cell
+    
+    private var urlString = ""
+    
+    
     func configureCell(with urlString: String) {
+        // set the cell's urlString
+        self.urlString = urlString
         
         planetImageView.getImage(with: urlString) { result in
             switch result {
@@ -21,10 +30,21 @@ class PlanetCell: UITableViewCell {
                 }
             case .success(let image):
                 DispatchQueue.main.async {
-                    self.planetImageView.image = image
+                    
+                    // only if the cell's urlString is the same as the one being passed in from the cellForRowAt will the current image view's image change
+                    if self.urlString == urlString {
+                        self.planetImageView.image = image
+                    }
                 }
             }
         }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        // empty out the image view; set it to nil
+        planetImageView.image = nil
+        
     }
 }
 
